411e7d26934ec5a722bed630bbe0c2dcb30b4992
Dan Bergelt <dan@danbergelt.com>
2021-10-03 19:40:54 -0400

    tweak

--- init.sh
+++ init.sh
@@ -7,10 +7,12 @@ init() {
 
 
 if [[ $1 = "-f" ]]; then
+  echo
   init
 else
   read -p "This may ovewrite files in your home directory. Are you sure? (y/n) " -n 1;
   if [[ $REPLY = "y" ]]; then
+    echo
     init
   fi;
 fi;
