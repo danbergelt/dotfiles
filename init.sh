b61d5e604be5243c3e239e64b1b86c8e8d853ae6
danbergelt <dan@danbergelt.com>
2021-10-03 20:12:35 -0400

    typo

--- init.sh
+++ init.sh
@@ -11,7 +11,7 @@ if [[ $1 = "-f" ]]; then
   echo
   init
 else
-  read -p "This may ovewrite files in your home directory. Are you sure? (y/n) " -n 1;
+  read -p "This may overwrite files in your home directory. Are you sure? (y/n) " -n 1;
   if [[ $REPLY = "y" ]]; then
     echo
     init
