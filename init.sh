28779a542afaa8201e0a33532ea462c64f384f0c
Dan Bergelt <dan@danbergelt.com>
2021-10-06 19:22:46 -0400

    simplify init script

--- init.sh
+++ init.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
 
-init() {
-  echo
-  rsync --exclude={"README.md","init.sh",".git/"} -avh --no-perms . ~
-  vim +'PlugInstall --sync' +qall &> /dev/null
-}
-
-
-if [[ $1 = "-f" ]]; then
-  init
-else
-  read -p "This may overwrite files in your home directory. Are you sure? (y/n) " -n 1;
-  if [[ $REPLY = "y" ]]; then
-    init
-  fi;
+read -p "This may overwrite files in your home directory. Are you sure? (y/n) " -n 1;
+if [[ $REPLY = "y" ]]; then
+        echo
+        rsync --exclude={"README.md","init.sh",".git/"} -avh --no-perms . ~
+        vim +'PlugInstall --sync' +qall &> /dev/null
 fi;
-
-
-unset init;
