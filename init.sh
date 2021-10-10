d93a4e35ed1728a0a3d0e6adbfe7368b269e4653
Dan Bergelt <dan@danbergelt.com>
2021-10-10 14:26:08 -0400

    move them to plugins, clean

--- init.sh
+++ init.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 #
-# 1. Sync files
-# 2. Install vim plugins
+# Entrypoint
 #
 
 read -p "This may overwrite files in your home directory. Are you sure? (y/n) " -n 1;
 if [[ $REPLY = "y" ]]; then
         echo
+        # 1. Sync files
         rsync --exclude={"README.md","init.sh",".git/"} -avh --no-perms . ~
+        # 2. Download vim plugin manager
+        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        # 3. Download all vim plugins
         vim +'PlugInstall --sync' +qall &> /dev/null
 fi;
