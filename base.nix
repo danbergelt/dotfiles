7acbe2a62fc1e4d55aa7e293e0f49dbd0ff08ae1
Dan <dan@danbergelt.com>
2023-08-11 19:47:03 -0400

    keep everything in nix file

--- base.nix
+++ base.nix
@@ -2,6 +2,7 @@
 
 # https://lazamar.co.uk/nix-versions/ to generate archives (useful for pinning old package versions)
 let
+  # 03/11/2023
   archive = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8.tar.gz";
   }) {};
@@ -31,8 +32,66 @@ in
 
   home.file = {
     ".hushlogin".text = "";
-    ".tmux.conf".source = home/tmux.conf;
-    ".bash_profile".source = home/bash_profile;
+  };
+
+  programs.bash = {
+    enable = true;
+
+    sessionVariables = {
+      EDITOR = "hx";
+      GIT_EDITOR = "hx";
+      COLORTERM = "truecolor";
+    };
+
+    shellAliases = {
+      hm = "home-manager";
+    };
+
+    initExtra = ''
+      NIXPRF=~/.nix-profile/etc/profile.d/nix.sh
+      [[ -s $NIXPRF ]] && . $NIXPRF
+
+      PS1="\w\[\033[36m\]\$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ {\1}/')\[\033[00m\] :: "
+
+      if command -v fzf-share > /dev/null; then
+        . "$(fzf-share)/key-bindings.bash"
+        . "$(fzf-share)/completion.bash"
+      fi
+
+      # Local overrides
+      BSHLCL=~/.bash_local
+      [[ -s $BSHLCL ]] && . $BSHLCL
+    '';
+   
+  };
+
+  programs.tmux = {
+    enable = true;
+
+    historyLimit = 5000;
+    terminal = "screen-256color";
+    keyMode = "vi";
+
+    extraConfig = ''
+      set -g mouse on
+      set -g pane-active-border-style fg=green
+      set -g status-right '%A, %d %b %Y %I:%M %p'
+      
+      bind c new-window -c '#{pane_current_path}'
+      bind x split-window -h -c '#{pane_current_path}'
+      bind y split-window -c '#{pane_current_path}'
+      
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+      
+      bind R respawn-pane -k -c '#{pane_current_path}'
+      
+      bind -T copy-mode-vi v send -X begin-selection
+      bind -T copy-mode-vi y send -X copy-pipe-and-cancel 'clip.exe'
+      bind -T copy-mode-vi Enter send -X cancel
+    '';
   };
 
   programs.helix = {
