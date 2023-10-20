aaa6258fa039337e01ceb32e039cd384900373a1
danbergelt <dan@danbergelt.com>
2023-10-20 00:45:35 -0400

    use american date fmt

--- base.nix
+++ base.nix
@@ -64,7 +64,7 @@ in
     extraConfig = ''
       set -g mouse on
       set -g pane-active-border-style fg=green
-      set -g status-right '%A, %d %b %Y %I:%M %p'
+      set -g status-right '%I:%M %p - %A, %b %d, %Y'
       
       bind c new-window -c '#{pane_current_path}'
       bind x split-window -h -c '#{pane_current_path}'
