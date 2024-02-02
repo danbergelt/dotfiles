36e45c0fe825a62e6fb85c81f818b7ed02265816
danbergelt <dan@danbergelt.com>
2024-02-02 14:37:05 -0500

    simplify setup script

--- setup.sh
+++ setup.sh
@@ -39,61 +39,13 @@ assert_exists() {
   fi
 }
 
-get_user_confirmation() {
-  if test -n "$FORCE"; then
-    return
-  fi
-
-  read -p "Bootstrap dotfiles? (y/n) " choice
-
+ask() {
+  read -p "$1" choice
   case "$choice" in
     y|Y) return ;;
     n|N) exit 0 ;;
-    *) get_user_confirmation ;;
+    *) ask "$1" ;;
   esac
-}
-
-clean() {
-  rm -f "$PROFILE_PATH"
-  rm -rf "$REPO_LOCATION" 
-}
-
-install_nix() {
-  sh <(curl -L https://nixos.org/nix/install) --no-daemon
-  source "$HOME/.nix-profile/etc/profile.d/nix.sh"
-}
-
-install_home_manager() {
-  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-  nix-channel --update
-  nix-shell '<home-manager>' -A install
-}
-
-clone_dotfiles_repo() (
-  git clone "https://$UPSTREAM" "$REPO_LOCATION"
-  cd "$REPO_LOCATION"
-
-  if test -n "$GITHUB_TOKEN"; then
-    git remote remove origin
-    git remote add origin "https://$GITHUB_TOKEN@$UPSTREAM"
-  fi
-
-  if test -n "$GIT_USERNAME"; then
-    git config user.name "$GIT_USERNAME"
-  fi
-
-  if test -n "$GIT_EMAIL"; then
-    git config user.email "$GIT_EMAIL"
-  fi
-)
-
-expose_dotfiles_config() {
-  local target import
-
-  target="enable = true;"
-  import="imports = [$REPO_LOCATION];"
-
-  sed -i "s:$target:$target\n\n  $import:" "$PROFILE_PATH"
 }
 
 while test $# -ne 0; do
@@ -110,12 +62,45 @@ done
 
 assert_exists git
 assert_exists curl
-get_user_confirmation
-clean
-install_nix  
-install_home_manager
-clone_dotfiles_repo
-expose_dotfiles_config
+
+if test -z "$FORCE"; then
+  ask "Setup dotfiles? (y/n) "
+fi
+
+# TODO: remove
+rm -f "$PROFILE_PATH"
+rm -rf "$REPO_LOCATION"
+
+# Install nix and home-manager
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+source "$HOME/.nix-profile/etc/profile.d/nix.sh"
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
+
+# TODO: do not clone if already exists, just fetch
+git clone "https://$UPSTREAM" "$REPO_LOCATION"
+
+pushd "$REPO_LOCATION"
+if test -n "$GITHUB_TOKEN"; then
+  git remote remove origin
+  git remote add origin "https://$GITHUB_TOKEN@$UPSTREAM"
+fi
+
+if test -n "$GIT_USERNAME"; then
+  git config user.name "$GIT_USERNAME"
+fi
+
+if test -n "$GIT_EMAIL"; then
+  git config user.email "$GIT_EMAIL"
+fi
+popd
+
+# TODO: do not write if already exists
+target="enable = true;"
+import="imports = [$REPO_LOCATION];"
+sed -i "s:$target:$target\n\n  $import:" "$PROFILE_PATH"
+
 home-manager switch
 
 echo "All done, please reload your shell"
