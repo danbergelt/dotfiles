38cc8888b9aacfa76a33b95349bd628b2f50994e
danbergelt <dan@danbergelt.com>
2024-02-23 18:58:57 -0500

    lower tmux escape time

--- nix/tmux.nix
+++ nix/tmux.nix
@@ -14,6 +14,7 @@ in
 
     extraConfig = ''
       set -g mouse on
+      set -g escape-time 10
       set -g pane-active-border-style fg=green
       set -g status-right '%I:%M %p - %A, %b %d %Y'
       
