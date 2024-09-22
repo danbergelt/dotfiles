e548b50f7315391c780e2e9f4581913b69c4ef12
danbergelt <dan@danbergelt.com>
2024-09-22 12:51:23 -0400

    add shell function

--- nix/bash.nix
+++ nix/bash.nix
@@ -17,9 +17,30 @@
 
     initExtra = ''
       source ~/.nix-profile/etc/profile.d/nix.sh
+
       source ${pkgs.git}/share/git/contrib/completion/git-prompt.sh
       PS1="\w\[\e[01;36m\]\$(__git_ps1)\[\e[00m\] :: "
-      source ~/.overrides 2> /dev/null # MUST BE LAST
+
+      # Open a Nix shell for the provided program
+      shell() {
+        if test -z "$1"; then
+          echo "Usage: shell <program>"
+          return 1
+        fi
+
+        # Place custom shells in ~/shells
+        program="$HOME/shells/$1.nix"
+
+        # Pass through to Nix package if no shell file found
+        if ! test -f "$program"; then
+          program="-p $1"
+        fi
+
+        NIX_PKGS_ALLOW_INSECURE=1 nix-shell --pure "$program"
+      }
+
+      ######### MUST BE LAST #########
+      source ~/.overrides 2> /dev/null
     '';
   };
 
