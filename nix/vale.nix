8cf85f80ad8d2d5c2d251b472245a6aa097a8eb2
danbergelt <dan@danbergelt.com>
2025-02-01 16:02:55 -0500

    expose accept/reject file paths in environment

--- nix/vale.nix
+++ nix/vale.nix
@@ -1,10 +1,16 @@
 { pkgs, lib, ... }:
 
+let
+  stylesPath = ".vale/styles";
+  vocabPath = "$HOME/${stylesPath}/config/vocabularies/Global";
+in
+
 {
   home.packages = [pkgs.vale];
 
   # Config file
   home.file.".vale.ini".text = ''
+    StylesPath = ${stylesPath}
     MinAlertLevel = suggestion
     Packages = Microsoft
     Vocab = Global
@@ -17,7 +23,17 @@
     Microsoft.Acronyms = NO
   '';
 
-  # Install + sync in one step
+  # Sync on every install
   home.activation.valeSync =
     lib.hm.dag.entryAfter ["installPackages"] "${pkgs.vale}/bin/vale sync";  
+
+  # Ensure global vocab path exists
+  home.activation.valeMkVocab =
+    lib.hm.dag.entryAfter ["installPackages"] "mkdir -p ${vocabPath}";
+
+  # Expose accept/reject file paths
+  home.sessionVariables = {
+    VOCAB_ACCEPT_FILE="${vocabPath}/accept.txt";
+    VOCAB_REJECT_FILE="${vocabPath}/reject.txt";
+  };
 }
