6983d35a19432c802ba89d7d999f5d1296553ced
danbergelt <dan@danbergelt.com>
2025-12-27 16:57:39 -0500

    fix nix-shell prompt

--- nix/bash.nix
+++ nix/bash.nix
@@ -9,6 +9,7 @@
       GIT_EDITOR = "hx";
       COLORTERM = "truecolor";
       BAT_THEME = "base16";
+      NIX_SHELL_PRESERVE_PROMPT = 1;
     };
 
     shellAliases = {
@@ -23,7 +24,10 @@
       source ~/.nix-profile/etc/profile.d/nix.sh
 
       source ${pkgs.git}/share/git/contrib/completion/git-prompt.sh
-      PS1="\w\[\e[01;36m\]\$(__git_ps1)\[\e[00m\] :: "
+      colored_git_ps1="\[\e[01;36m\]\$(__git_ps1)\[\e[00m\]"
+      nix_shell="$(test -n "$IN_NIX_SHELL" && echo "<nix-shell> ")"
+      colored_nix_shell="\[\e[01;35m\]\$nix_shell\[\e[00m\]"
+      PS1="$colored_nix_shell\w$colored_git_ps1 :: "
 
       # Activate Node from version manager
       XDG_RUNTIME_DIR=/tmp/run/user/$(id -u)
