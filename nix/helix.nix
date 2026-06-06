9c3f00ed58cf9a9c4a265dfea63498023398038c
danbergelt <dan@danbergelt.com>
2026-06-06 15:19:50 -0400

    pin helix

--- nix/helix.nix
+++ nix/helix.nix
@@ -1,8 +1,17 @@
 { pkgs, ... }:
 
+let
+  # 25.01
+  pinned = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/de0fe301211c267807afd11b12613f5511ff7433.tar.gz";
+    sha256 = "0lw17azfdh3gmh75ddha01j4c7fn38nz4w3jwzbaz0ngb8nchb3a";
+  }) { inherit (pkgs) system; };
+in
+
 {
   programs.helix = {
     enable = true;
+    package = pinned.helix;
 
     extraPackages = with pkgs; [
       nil
